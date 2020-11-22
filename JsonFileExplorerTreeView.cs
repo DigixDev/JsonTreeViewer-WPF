a96b7afa3dfdf652cc2c8ba1a8462e980b1839a0
DigixDev <71383191+DigixDev@users.noreply.github.com>
2020-11-22 14:51:28 +0300

    Add files via upload

---
+++ JsonFileExplorerTreeView.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Web.Script.Serialization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+
+namespace Comae.Controls
+{
+    public class JsonFileExplorerTreeView : TreeView
+    {
+        public string RootFolder
+        {
+            get { return (string)GetValue(RootFolderProperty); }
+            set { SetValue(RootFolderProperty, value); }
+        }
+
+        public string SelectedFileName
+        {
+            get { return (string)GetValue(SelectedFileNameProperty); }
+            set { SetValue(SelectedFileNameProperty, value); }
+        }
+
+        public static readonly DependencyProperty SelectedFileNameProperty = DependencyProperty.Register("SelectedFileName", typeof(string), typeof(JsonFileExplorerTreeView), new PropertyMetadata(""));
+        public static readonly DependencyProperty RootFolderProperty = DependencyProperty.Register("RootFolder", typeof(string), typeof(JsonFileExplorerTreeView), new PropertyMetadata("", OnDataChanged));
+
+        private static void OnDataChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var p = d as JsonFileExplorerTreeView;
+            p.StartBrowsing(e.NewValue?.ToString());
+        }
+
+        private void StartBrowsing(string path)
+        {
+            Items.Clear();
+
+            if (string.IsNullOrEmpty(path) == false)
+                PopulateDirectoryList(path, null);
+        }
+
+        private void PopulateDirectoryList(string path, TreeViewItem parent)
+        {
+            var dirs = Directory.GetDirectories(path);
+            foreach (var dir in dirs)
+            {
+                var node = MakeNode(dir.ToLower(), parent, true);
+                PopulateDirectoryList(dir, node);
+            }
+
+            var files = Directory.GetFiles(path);
+            foreach (var file in files)
+            {
+                var name = file.ToLower();
+                if (name.EndsWith(".json"))
+                    MakeNode(name.ToLower(), parent, false);
+                else if(name.EndsWith(".zip"))
+                {
+                    var node = MakeNode(name.ToLower(), parent, false);
+                    using (var zipFile = ZipFile.OpenRead(name))
+                    {
+                        foreach (var item in zipFile.Entries)
+                            MakeComplexNodes(item.FullName, name,  node);
+                    }
+                }
+            }
+        }
+
+        private void MakeComplexNodes(string fullName, string zipFileName, TreeViewItem parent)
+        {
+            TreeViewItem temp = null;
+            var node = parent;
+            var parts = fullName.Split('/');
+            for (int i = 0; i < parts.Length; i++)
+            {
+                if (i == parts.Length - 1)
+                {
+                    var child = MakeNode(fullName, node, false, zipFileName);
+                }
+                else
+                {
+                    temp = null;
+                    foreach (TreeViewItem item in node.Items)
+                    {
+                        var tag = item.Tag as dynamic;
+                        if (tag.Name.Equals(parts[i]))
+                        {
+                            temp = item;
+                            break;
+                        }
+                    }
+                    if (temp != null)
+                        node = temp;
+                    else
+                    {
+                        node = MakeNode(parts[i], node, true);
+                    }
+                }
+            }
+        }
+
+        private TreeViewItem MakeNode(string fullName ,TreeViewItem parent, bool isDirectory,  string zipFileName="" )
+        {
+            var name = "";
+            var node = new TreeViewItem();
+            var panel = new StackPanel()
+            {
+                Orientation = Orientation.Horizontal
+            };
+            var image = new Image()
+            {
+                Width = 16,
+                Height = 16,
+            };
+
+            if (isDirectory)
+            {
+                image.Source = new BitmapImage(new Uri("/Images/folder.png", UriKind.Relative));
+                name = fullName.Substring(fullName.LastIndexOf('\\') + 1);
+            }
+            else
+            {
+                if (fullName.EndsWith(".json"))
+                    image.Source = new BitmapImage(new Uri("/Images/json.png", UriKind.Relative));
+                else
+                    image.Source = new BitmapImage(new Uri("/Images/rar.png", UriKind.Relative));
+                name = Path.GetFileName(fullName);
+            }
+
+            var text = new TextBlock()
+            {
+                Text = name,
+                FontSize = 10,
+                VerticalAlignment = System.Windows.VerticalAlignment.Center,
+                Margin = new System.Windows.Thickness(3, 1, 1, 1)
+            };
+
+            panel.Children.Add(image);
+            panel.Children.Add(text);
+
+            node.Header = panel;
+            node.Tag = new
+            {
+                Name = name,
+                FullName = fullName,
+                IsDirectory = isDirectory,
+                ZipFileName = zipFileName
+            };
+
+            node.Cursor = Cursors.Hand;
+
+            if (parent == null)
+                Items.Add(node);
+            else
+                parent.Items.Add(node);
+            return node;
+        }
+
+        protected override void OnSelectedItemChanged(RoutedPropertyChangedEventArgs<object> e)
+        {
+            var item = ((TreeViewItem)e.NewValue);
+            var tag = item.Tag as dynamic;
+            if (tag.IsDirectory == false)
+            {
+                item.Cursor = Cursors.Wait;
+                if (String.IsNullOrEmpty(tag.ZipFileName))
+                    SelectedFileName = tag.FullName;
+                else
+                    SelectedFileName = $"zip:{tag.ZipFileName},{tag.FullName}";
+                item.Cursor = Cursors.Hand;
+            }
+        }
+
+        public JsonFileExplorerTreeView()
+        {
+
+        }
+    }
+}
---
+++ JsonTreeView.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Text.RegularExpressions;
+using System.Web.Script.Serialization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+/*  Code by: Mehran Sivari  */
+namespace App.Controls
+{
+	public class JsonTreeView: TreeView
+    {
+        #region properties
+
+        public string JsonFileName
+		{
+			get { return (string)GetValue(JsonFileNameProperty); }
+			set { SetValue(JsonFileNameProperty, value); }
+		}
+
+		public string JsonText
+		{
+			get { return (string)GetValue(JsonTextProperty); }
+			set { SetValue(JsonTextProperty, value); }
+		}
+
+		private static void OnJsonTextChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+		{
+			var obj = d as JsonTreeView;
+			obj.PopulateTreeItems((string)e.NewValue);
+		}
+		
+		private static void OnJsonFileNameChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+		{
+			var obj = d as JsonTreeView;
+
+			var fileName = (string)e.NewValue;
+			if (string.IsNullOrEmpty(fileName))
+				return;
+
+			if (fileName.EndsWith(".json") == false)
+				return;
+
+			if (fileName.StartsWith("zip:"))
+				obj.JsonText = ReadZipFile(fileName);
+			else
+				obj.JsonText = File.ReadAllText(fileName);
+		}
+
+		public static readonly DependencyProperty JsonTextProperty = DependencyProperty.Register("JsonText", typeof(string), typeof(JsonTreeView), new PropertyMetadata("", OnJsonTextChanged));
+		public static readonly DependencyProperty JsonFileNameProperty = DependencyProperty.Register("JsonFileName", typeof(string), typeof(JsonTreeView), new PropertyMetadata("", OnJsonFileNameChanged));
+
+		#endregion
+		#region serializers
+
+		private Dictionary<string, object> DeserializeArrayList(string fileName)
+		{
+			try
+			{
+				JavaScriptSerializer serializer = new JavaScriptSerializer();
+
+				var array = serializer.Deserialize<ArrayList>(JsonText) as ArrayList;
+				serializer.MaxJsonLength = Int32.MaxValue;
+
+				return ConvertArray(array);
+			}
+			catch (Exception ex)
+			{
+				return null;
+			}
+		}
+
+		private Dictionary<string, object> DeserializeDictionary(string fileName)
+		{
+			try
+			{
+				JavaScriptSerializer serializer = new JavaScriptSerializer();
+				serializer.MaxJsonLength = Int32.MaxValue;
+
+				return serializer.Deserialize<Dictionary<string, object>>(JsonText) as Dictionary<string, object>;
+			}
+			catch (Exception)
+			{
+				return null;
+			}
+		}
+
+        #endregion
+        #region methods
+
+        private static string ReadZipFile(string fileName)
+		{
+			var zipFileName = "";
+			var subFileName = "";
+			var mc = Regex.Match(fileName, @"^zip:(?<zipFileName>.*?),(?<subFileName>.*?)$");
+			if (mc.Success)
+			{
+				zipFileName = mc.Groups["zipFileName"].Value;
+				subFileName = mc.Groups["subFileName"].Value;
+				var zipFile = ZipFile.OpenRead(zipFileName);
+				foreach (var item in zipFile.Entries)
+				{
+					if (item.FullName == subFileName)
+					{
+						using (var reader = new StreamReader(item.Open()))
+						{
+							return reader.ReadToEnd();
+						}
+					}
+				}
+			}
+
+			return string.Empty;
+		}
+
+		public void PopulateTreeItems(string text)
+		{
+			try
+			{
+				Items.Clear();
+
+				var dics = DeserializeDictionary(text);
+				if(dics!=null)
+				{
+					MakeNodes(dics, null);
+				}
+				else
+				{
+					var temp = DeserializeArrayList(text);
+					if (temp != null)
+					{
+						MakeNodes(temp, null);
+					}
+					else
+						throw new Exception("Error");
+				}
+			}
+			catch (Exception ex)
+			{
+				throw;
+			}
+		}
+
+		private void MakeNodes(Dictionary<string, object> data, TreeViewItem parent)
+		{
+			foreach(var key in data.Keys)
+			{
+				if(data[key] is Dictionary<string, object>)
+				{
+					var node = MakeTreeNode(key, parent, false);
+					MakeNodes(data[key] as Dictionary<string, object>, node);
+				}
+				else if (data[key] is ArrayList)
+				{
+					var node = MakeTreeNode(key, parent, false);
+					MakeNodes(ConvertArray(data[key]), node);
+				}
+				else
+				{
+					 MakeTreeNode($"{key} : \"{data[key]}\"", parent, true);
+				}
+			}
+		}
+
+		private TreeViewItem MakeTreeNode(string header, TreeViewItem parent, bool isLeaf = false)
+		{
+			var node = new TreeViewItem();
+			var panel = new StackPanel()
+			{
+				Orientation = Orientation.Horizontal
+			};
+			var image = new Image()
+			{
+				Width = 16,
+				Height = 16,
+			};
+
+			if (isLeaf)
+				image.Source = new BitmapImage(new Uri("/Images/leaf.png", UriKind.Relative));
+			else
+				image.Source = new BitmapImage(new Uri("/Images/node.png", UriKind.Relative));
+
+			var text = new TextBlock()
+			{
+				Text = header,
+				FontSize = 10,
+				VerticalAlignment = System.Windows.VerticalAlignment.Center,
+				Margin = new System.Windows.Thickness(3, 1, 1, 1)
+			};
+
+			panel.Children.Add(image);
+			panel.Children.Add(text);
+
+			node.Header = panel;
+
+			if (parent == null)
+				Items.Add(node);
+			else
+				parent.Items.Add(node);
+			return node;
+		}
+
+		private Dictionary<string, object> ConvertArray(object array)
+		{
+			var index = 1;
+			var dics = new Dictionary<string, object>();
+			foreach (var item in array as ArrayList)
+			{
+				dics.Add(index.ToString(), item);
+				index++;
+			}
+			return dics;
+		}
+
+		#endregion
+	}
+}
